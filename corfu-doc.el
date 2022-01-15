f030d5817606602cdfdacb669c6d73f92f7b9b04
Yuwei Tian <ibluefocus@outlook.com>
2022-01-16 00:00:45 +0800

    Version 0.0.4

--- corfu-doc.el
+++ corfu-doc.el
@@ -4,7 +4,7 @@
 
 ;; Author: Yuwei Tian <ibluefocus@NOSPAM.gmail.com>
 ;; URL: https://github.com/galeo/corfu-doc
-;; Version: 0.0.3
+;; Version: 0.0.4
 ;; Keywords: corfu popup documentation convenience
 ;; Package-Requires: ((emacs "26.0")(corfu "0.16.0"))
 
